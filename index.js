9b1220da50e1c6f10153489d119788257a5f5fda
abhi <abhishekpandit1212@gmail.com>
2023-03-14 12:46:59 +0530

    build first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let string = "";
+let buttons = document.querySelectorAll(".button");
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    try {
+      if (e.target.innerHTML == "=") {
+        string = eval(string);
+        document.querySelector("input").value = string;
+      } else if (e.target.innerHTML == "AC") {
+        string = "";
+        document.querySelector("input").value = string;
+      } else if (e.target.innerHTML == "DEL") {
+        string = string.slice(0, -1);
+        document.querySelector("input").value = string;
+        console.log(string);
+      } else {
+        console.log(e.target);
+        string = string + e.target.innerHTML;
+        document.querySelector("input").value = string;
+      }
+    } catch (error) {
+      string = "Syntax error";
+      document.querySelector("input").value = string;
+    }
+  });
+});
